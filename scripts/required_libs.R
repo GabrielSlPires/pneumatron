d98d1d8dc7e5bdc2b3e314e57a23beebd0f0017d
GabrielSlPires <gabriel.slpires@gmail.com>
2022-08-02 08:20:12 +0200

    load libraries

--- scripts/required_libs.R
+++ scripts/required_libs.R
@@ -1,2 +1,8 @@
-c("serial",
-  "lubridate")
+library("serial")
+library("lubridate")
+library("dplyr")
+library("ggplot2")
+library("rstudioapi")
+
+source("scripts/lib/pneumatron_air_discharge.R", local = TRUE)
+source("scripts/lib/psi_extrapolation.R", local = TRUE)
