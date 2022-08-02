5f69ecd552bedfc40822f69e5ca7071ff1ae14f5
GabrielSlPires <gabriel.slpires@gmail.com>
2022-08-02 08:16:38 +0200

    plot curves that are currenly running

--- run.R
+++ run.R
@@ -7,3 +7,10 @@ file_name <- "2022_7_20" #define where to save your files. change for week files
 #To start reading the pneumatron
 jobRunScript("scripts/00-read_pneumatron.R")
 
+#To keep tracking current curves
+jobRunScript("scripts/01-running_curves.R",
+             workingDir = getwd(),
+             exportEnv = "R_GlobalEnv",
+             importEnv = TRUE)
+plot(p)
+
