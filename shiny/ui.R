8c38f1728df90a540c94157ead24c039d4f54abc
GabrielSlPires <gabriel.slpires@gmail.com>
2022-08-03 09:57:32 +0200

    creating sidebar panel

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,8 +1,33 @@
 library(shiny)
 library(shinydashboard)
 
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Experiments Managment",
+             tabName = "exp_managment_view",
+             badgeLabel = "new",
+             icon = icon("seedling") #filter-list
+    ),
+    menuItem("Running Experiments",
+             tabName = "running_view",
+             icon = icon("filter") #filter-list
+    ),
+    menuItem("Analysis",
+             tabName = "analysis_view",
+             icon = icon("chart-bar")
+    ),
+    menuItem("About",
+             icon = icon("th"),
+             tabName = "about_vire"
+    )
+  ),
+  menuItem("Source code",
+           icon = icon("file-code"),
+           href = "https://github.com/GabrielSlPires/pneumatron")
+)
+
 dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
+  dashboardHeader(title = "Pneumatron"),
+  sidebar,
   dashboardBody()
 )
