6d4c64e4c2e87dede2e0b02e3b5f29bde9726dc7
GabrielSlPires <gabriel.slpires@gmail.com>
2022-08-15 12:06:14 +0200

    automatically create folders

--- scripts/required_libs.R
+++ scripts/required_libs.R
@@ -8,9 +8,17 @@ packages <- c(
     "rstudioapi",
     "serial",
     "shiny",
-    "shinydashboard"
+    "shinydashboard",
+    "shinyTime"
 )
 
 install.packages(setdiff(packages, rownames(installed.packages())))
 
 rm(packages)
+
+dir.create("data", showWarnings = FALSE)
+dir.create("data/raw_pneumatron", showWarnings = FALSE)
+dir.create("data/raw_psi", showWarnings = FALSE)
+dir.create("data/raw_table", showWarnings = FALSE)
+dir.create("fig", showWarnings = FALSE)
+dir.create("result", showWarnings = FALSE)
