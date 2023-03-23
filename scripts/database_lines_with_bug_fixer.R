fd989da0c2b8fd54d00a60aad4e90b6012c2dc47
GabrielSlPires <gabriel.slpires@gmail.com>
2023-03-23 11:12:14 -0300

    fix cleaning script to work faster

--- scripts/database_lines_with_bug_fixer.R
+++ scripts/database_lines_with_bug_fixer.R
@@ -1,66 +1,45 @@
 #!/usr/bin/env Rscript
-# Receive file name from command line
+#Receive file name from command line - for future updates
 #file_name <- commandArgs(trailingOnly = TRUE)
 
-file_name <- "data/IAC/data_iac/pneumatron_database-v4.csv"
+#Database file to be cleaned
+file_name <- "data/IAC/pneumatron_database-03-06.csv"
+#File to save non matched measures
+file_trash <- "data/IAC/trash.csv"
 
-# exculir todas as linhas com [^ -:.,v0-9]
+#regex to only allowed formats
+#^(-?[0-9]+,){5}(([0-9]+\.[0-9]+,){4}v3|,([0-9]+\.[0-9]+),,,v1),[0-9]{4}(-[0-9]+){2} [0-9]+(:[0-9]+){2}
 
-#read all lines from file
+#R nedds to duplicate \ to understand this character
+allowed_regex <- "^(-?[0-9]+,){5}(([0-9]+\\.[0-9]+,){4}v3|,([0-9]+\\.[0-9]+),,,v1),[0-9]{4}(-[0-9]+){2} [0-9]+(:[0-9]+){2}"
+
+#read all lines from database file
 con <- file(file_name, "r", blocking = FALSE)
 data_to_correct <- readLines(con)
 close(con)
 
-non_allowed_characters <- stringr::str_detect(data_to_correct,
-                                              "[^ -:.,v0-9]",
-                                              negate = TRUE)
-#remove line with bug and rewrite file
+#match lines with regex format
+matched_lines <- stringr::str_detect(data_to_correct, allowed_regex)
+
+#write only lines which matched regex
 con <- file(file_name, "w")
-writeLines(data_to_correct[non_allowed_characters], con)
+writeLines(data_to_correct[matched_lines], con)
 close(con)
 
+#write only lines which mismatched regex
+con <- file(file_trash, "w")
+writeLines(data_to_correct[!matched_lines], con)
+close(con)
 
-if (length(file_name) == 0) stop("File name must be supplied (input file)", call. = FALSE)
-
-#I need to do it in a smarter/faster way.
-#This procedure works, but is time consuming as database starts to increase
-need_correction <- TRUE
-while (need_correction) {
-  need_correction <- tryCatch({
-    #Try to open database, if finds an line with bug it returns a warning
-    data <- data.table::fread(file_name, showProgress = FALSE)
-    need_correction <- FALSE #This line runs with no warning is triggered
-  }, warning = function(w) {
-    #Get the line number with error
-    line_to_remove <- stringr::str_extract(as.character(w),
-                                           "(?<=Stopped early on line )[0-9]*")
-    #When using fread() inside tryCatch, connection to file is left open
-    #reopen file with fread() properly closes file connection
-    try(data <- data.table::fread(file_name, nrows = 100), silent = TRUE)
-    
-    #read all lines from file
-    con <- file(file_name, "r", blocking = FALSE)
-    data_to_correct <- readLines(con)
-    close(con)
-    
-    #remove line with bug and rewrite file
-    con <- file(file_name, "w")
-    writeLines(data_to_correct[-as.numeric(line_to_remove)], con)
-    close(con)
-    
-    #message line number which was removed
-    message("removed line ", line_to_remove, ": ", data_to_correct[as.numeric(line_to_remove)]) #Mostrar conteudo da linha
-    
-    return(TRUE)
-  })
-}
+message("Removed ", sum(!matched_lines), " line(s) with different format than expected")
 
 #Clear environment
-rm(data,
+rm(file_name,
+   file_trash,
+   allowed_regex,
+   con,
    data_to_correct,
-   line_to_remove,
-   file_name,
-   need_correction)
+   matched_lines)
 
 
 message("\nAll lines with bugs were removed, database is ready for usage!\n")
