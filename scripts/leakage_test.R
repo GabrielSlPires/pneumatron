c186c94222d9cd9275e578931cb3821804f3ec86
GabrielSlPires <gabriel.slpires@gmail.com>
2024-03-19 08:59:43 -0300

    updated leakage_test.R

--- scripts/leakage_test.R
+++ scripts/leakage_test.R
@@ -1,43 +1,29 @@
 source("shiny/helper.R")
 library(ggplot2)
 library(dplyr)
-    
-while (TRUE) { # rodar nessa linha
-  try({
-    #data <- filter(open_pneumatron_db("data/raw_pneumatron/leak_test_03-15.csv"))
-    
-    #colocar no tryCacht pegar warning para deletar linha
-    data <- open_pneumatron_db("data/IAC/data_iac/pneumatron_database-v4.csv")
-    
-    data %>% 
-      filter(datetime >= lubridate::ymd_hm("2023-03-24 13:30"),
-             pressure < 85) %>% 
-      group_by(id) %>% 
-      mutate(measure2 = measure - min(measure)) %>% 
-      ggplot(aes(log_line,
-                 pressure,
-                 color = datetime,
-                 group = paste(measure, group))) +
-      geom_line() +
-      geom_vline(aes(xintercept = 3)) +
-      geom_vline(aes(xintercept = 30)) +
-      facet_wrap(~id, scales = "free") +
-      ylab("Pressure (kPa)") +
-      xlab("log line") +
-      ggtitle("Pressure difference inside each measurement by Pneumatron") +
-      theme_bw()
 
-    #print(p)
+data <- open_pneumatron_db("https://raw.githubusercontent.com/GabrielSlPires/pneumatron_workshop_2024_03/main/pneumatron_database.csv")
 
-    print(data %>% 
-            filter(log_line > 2) %>% # se tiver valor estranho, aumenta pra 3, ou 4
-            summarize(`pressure diff [kPa]` = max(pressure) - min(pressure), # 3 kPa max
-                      speed = round(`pressure diff [kPa]`/n(), 3), # 0.02 max
-                      .groups = "drop") %>% 
-            arrange(id, speed) %>% 
-            as.data.frame())
-    calculate_air_discharge(data)
-    
-  })
-  Sys.sleep(5)
-}
+ggplot(data, aes(log_line,
+         pressure,
+         color = datetime,
+         group = paste(measure, group))) +
+  geom_line() +
+  geom_vline(aes(xintercept = 3)) +
+  geom_vline(aes(xintercept = 30)) +
+  facet_wrap(~id, scales = "free") +
+  ylab("Pressure (kPa)") +
+  xlab("log line") +
+  ggtitle("Pressure difference inside each measurement by Pneumatron") +
+  theme_bw()
+
+data %>% 
+  filter(log_line > 2) %>% 
+  group_by(id, group, measure) %>% 
+  summarize(`pressure diff [kPa]` = max(pressure) - min(pressure), # 3 kPa max
+            speed = round(`pressure diff [kPa]`/n(), 3), # 0.02 max
+            datetime = first(datetime),
+            .groups = "drop") %>% 
+  arrange(id, datetime) %>% 
+  select(id, measure, `pressure diff [kPa]`, speed, datetime) %>% 
+  as.data.frame()
